68aca12557ffb54754f8052123c88b3e4e1f74d9
leszekpogorzelski <46164637+leszekpogorzelski@users.noreply.github.com>
2019-07-29 14:01:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'http://https://git.heroku.com/quiet-caverns-50867.git/v1/';
+  const trelloApiRoot = 'https://git.heroku.com/quiet-caverns-50867.git/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
